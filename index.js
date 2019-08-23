d205bde8610b043165bc8694da718bb2252930d1
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-23 18:35:42 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (arr) => {
+  
+  //return 0 if the array is empty
+  if (!arr.length) return 0;
+  
+  /*
+   * Takes the array, removes the first element, and
+   * sets it to currentItem
+   * arr = [1, 2, 3] ---> [2, 3]
+   * currentItem = 1
+   */
+  const currentItem = arr.shift();
+  
+  if (Array.isArray(currentItem)) {
+
+    /*
+     * if currentItem is another array,
+     * Ex. [2, 3]
+     *
+     * sumItems(currentItems) + sumItems(arr)
+     * or
+     * sumItems([2, 3]) + sumItems([1, 2, 3]);
+     */
+    return sumItems(currentItem) + sumItems(arr);
+  }
+  
+  //if currentItem isnt an array, then return currentItem + the rest of the array
+  return currentItem + sumItems(arr);
+};
 
 module.exports = sumItems;
